fff7fb1373ca29f4c415b5846358dea72844854b
lnesh <107777140+lnesh@users.noreply.github.com>
2024-01-02 14:01:59 +0200

    updated connect.php

--- root/connect.php
+++ root/connect.php
@@ -1,12 +1,14 @@
 <?php
 $servername = "localhost";
+$dbname = "neolearn"
 $username = "root";
 $password = "";
 
-$conn = mysqli_connect($servername, $username, $password);
+$conn = mysqli_connect($servername, $username, $password $dbname);
 
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully";
+
+return $conn;
 ?> 
