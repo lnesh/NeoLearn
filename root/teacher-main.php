4a46a4e33c2dfe7991d2a0776e8044c7e5496034
lnesh <107777140+lnesh@users.noreply.github.com>
2024-01-08 17:09:37 +0200

    logout

--- root/teacher-main.php
+++ root/teacher-main.php
@@ -116,7 +116,11 @@
                                 <li>
                                     <hr class="dropdown-divider">
                                 </li>
-                                <li><a class="dropdown-item" href="#">Log out</a></li>
+
+                                <form action="./backend/logout.php">
+                                    <li><input type="submit" class="dropdown-item" value="Logout"></li>
+                                </form>
+                                
                             </ul>
                         </li>
                     </ul>
