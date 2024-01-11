3c9caf769141119128ebc39bd8e1b68302bae81c
lnesh <107777140+lnesh@users.noreply.github.com>
2024-01-11 08:57:32 +0200

    stlyle

--- root/teacher-main.php
+++ root/teacher-main.php
@@ -54,7 +54,8 @@ if (!isset($_SESSION['mail'])) {
                             d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                     </svg></button>
             </form>
-            <a style="background-color:white; color:purple; border: solid 3px;" class="btn btn-secondary" href="#">Add
+            <a style="background-color:white; color:purple; border: solid 3px;" class="btn btn-secondary"
+                href="teacher-add-course.php">Add
                 course</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar">
                 <svg fill="#EDF" width="40px" height="40px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -94,27 +95,7 @@ if (!isset($_SESSION['mail'])) {
                             </a>
                         </li>
 
-                        <li class="nav-item">
-                            <a href="#instructors" class="nav-link menu-text">
-                                <svg xmlns="http://www.w3.org/2000/svg" height="30" width="30" fill="#EDF"
-                                    viewBox="0 0 640 512">
-                                    <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.-->
-                                    <path
-                                        d="M160 64c0-35.3 28.7-64 64-64H576c35.3 0 64 28.7 64 64V352c0 35.3-28.7 64-64 64H336.8c-11.8-25.5-29.9-47.5-52.4-64H384V320c0-17.7 14.3-32 32-32h64c17.7 0 32 14.3 32 32v32h64V64L224 64v49.1C205.2 102.2 183.3 96 160 96V64zm0 64a96 96 0 1 1 0 192 96 96 0 1 1 0-192zM133.3 352h53.3C260.3 352 320 411.7 320 485.3c0 14.7-11.9 26.7-26.7 26.7H26.7C11.9 512 0 500.1 0 485.3C0 411.7 59.7 352 133.3 352z" />
-                                </svg><span class=" ms-4 d-none d-sm-inline ">Instructors</span>
-                            </a>
-                        </li>
 
-                        <li class="nav-item">
-                            <a href="#saved" class="nav-link  menu-text">
-                                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30"
-                                    fill="#EDF" viewBox="0 0 48 48">
-                                    <path
-                                        d="M 16.5 5 C 12.916 5 10 7.916 10 11.5 L 10 41.5 C 10 42.063 10.315406 42.579938 10.816406 42.835938 C 11.317406 43.092937 11.918953 43.045797 12.376953 42.716797 L 22.039062 35.759766 C 22.024062 35.506766 22 35.257 22 35 C 22 27.832 27.832 22 35 22 C 36.034 22 37.035 22.134281 38 22.363281 L 38 11.5 C 38 7.916 35.084 5 31.5 5 L 16.5 5 z M 35 24 C 28.925 24 24 28.925 24 35 C 24 41.075 28.925 46 35 46 C 41.075 46 46 41.075 46 35 C 46 28.925 41.075 24 35 24 z M 35 28 C 35.48 28 35.908453 28.305766 36.064453 28.759766 L 37.177734 32 L 40.875 32 C 41.358 32 41.787406 32.308625 41.941406 32.765625 C 42.095406 33.223625 41.939687 33.729484 41.554688 34.021484 L 38.560547 36.292969 L 39.574219 39.539062 C 39.720219 40.005063 39.548391 40.510969 39.150391 40.792969 C 38.955391 40.930969 38.727 41 38.5 41 C 38.263 41 38.025172 40.925391 37.826172 40.775391 L 35 38.660156 L 32.173828 40.775391 C 31.782828 41.068391 31.248609 41.076922 30.849609 40.794922 C 30.451609 40.512922 30.279781 40.005063 30.425781 39.539062 L 31.439453 36.294922 L 28.445312 34.021484 C 28.060312 33.729484 27.904594 33.225578 28.058594 32.767578 C 28.213594 32.309578 28.642 32 29.125 32 L 32.822266 32 L 33.935547 28.759766 C 34.091547 28.305766 34.52 28 35 28 z">
-                                    </path>
-                                </svg><span class=" ms-4 d-none d-sm-inline ">Saved</span>
-                            </a>
-                        </li>
 
                         <li class="nav-item disabled">
                             <a href="#notes" class="nav-link menu-text ">
