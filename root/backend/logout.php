73c27787ff20b31151d984416ca18112da282281
lnesh <107777140+lnesh@users.noreply.github.com>
2024-01-12 23:56:02 +0200

    logout fix

--- root/backend/logout.php
+++ root/backend/logout.php
@@ -4,7 +4,7 @@ session_start();
 if (isset($_SESSION['mail'])) {
     unset($_SESSION['mail']);
     session_destroy();
-    header('Location: ../login.php');
+    header('Location: ../index.html');
 } else {
 
     header("Location: ../login.php");
