38eb91436809db40a6c9cdf8abfc0931ab40d49a
christsarcho <ics21072@uom.edu.gr>
2024-01-13 15:53:11 +0200

    Update connect.php

--- root/backend/connect.php
+++ root/backend/connect.php
@@ -1,5 +1,12 @@
 <?php
-    $conn = mysqli_connect('localhost', 'root', '', 'neolearn', '3307' ) ;
-    if(!$conn){
-        echo 'Connection failed' .mysqli_connect_error();
+    // Assuming the MySQL server is on the default port 3306
+    $conn = mysqli_connect('localhost', 'root', '', 'neolearn');
+
+    // Check connection
+    if (!$conn) {
+        // Output a more detailed error message
+        die('Connection failed: ' . mysqli_connect_error());
+    } else {
+        echo 'Connected successfully';
     }
+?>
